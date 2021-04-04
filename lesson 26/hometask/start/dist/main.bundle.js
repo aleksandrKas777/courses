6c2da5e82374fdd541da4fe55d39a483088b8cfa
aleksandrKas777 <81757942+aleksandrKas777@users.noreply.github.com>
2021-04-04 15:46:57 +0300

    написал функции

---
+++ lesson 26/hometask/start/dist/main.bundle.js
@@ -0,0 +1,1 @@
+!function(o){let l=o.slice(0);l.sort(),console.log(o,l)}([4,1,0,6]);
--- lesson 26/hometask/start/src/array-utils.js
+++ lesson 26/hometask/start/src/array-utils.js
@@ -1,1 +1,15 @@
-console.log('4645677');
+function ascending(arr) {
+    const newArr = arr.slice(0);
+    newArr.sort();
+    console.log(arr, newArr);
+}
+
+
+function descending(arr) {
+    const newArr = arr.slice(0);
+    newArr.sort((a, b) => b - a);
+    console.log(arr, newArr);
+    
+}
+ascending([4,1,0,6]);
+descending([4,1,0,6]);
