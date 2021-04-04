bde9b5458f9d810dccd69946ef974b46c610f960
aleksandrKas777 <81757942+aleksandrKas777@users.noreply.github.com>
2021-04-04 14:47:00 +0300

    настроили сервер

--- lesson 26/hometask/start/webpack.config.js
+++ lesson 26/hometask/start/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 const conf = {
     entry: {
@@ -8,6 +10,23 @@ const conf = {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+
+
 
 }
 
